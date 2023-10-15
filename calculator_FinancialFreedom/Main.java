e9808b486d5dcdf9653437c54208c0cb55f2a005
GustavoSept <GustavoSept.Cron@gmail.com>
2023-10-15 19:42:44 -0300

    Added 'Salary Share' operation functionality

--- calculator_FinancialFreedom/Main.java
+++ calculator_FinancialFreedom/Main.java
@@ -9,7 +9,7 @@ public class Main {
         float yield_investment = 0.10f; // Yield Rate during investment period
         float yield_dividend = 0.03f; // Yield Rate after conquering Financial Freedom (recommend between 0.02 ~0.05)
         float investment_years = 20f;
-        String operation = "Yield"; // possible values: "Salary Share", "Yield", "Time", "Net Worth"
+        String operation = "Salary Share"; // possible values: "Salary Share", "Yield", "Time", "Net Worth"
         
 
         final byte MONTHS_IN_YEARS = 12;
@@ -20,7 +20,7 @@ public class Main {
         // Variables related to the switch/case operations
         double tentative_result = 0;
         double tentative_dividends = 0;
-        final byte ITER_MAX = 25;
+        final byte ITER_MAX = 120;
 
         switch (operation){
             case "Net Worth": // Calculating final Net Worth based on User Input
@@ -44,10 +44,10 @@ public class Main {
                     for(int i=0; i<=ITER_MAX; i++){
                         CompoundProjector yieldOBJ = new CompoundProjector(initial_Investment, salary, monthlySalaryShare, yieldTentative, yield_dividend, investment_years);
                         tentative_result = yieldOBJ.projectNetWorth();
+                        tentative_dividends = yieldOBJ.projectDividends();
                         
 
                         if (NEEDED_NETWORTH * 0.99999 < tentative_result && tentative_result < NEEDED_NETWORTH * 1.00001){ // Almost Equals, with 0.002% Threshold
-                            tentative_dividends = yieldOBJ.projectDividends();
                             System.out.println("This calculation took " + i + " iterations.");
                             System.out.println("Estimated Yield: " + Math.round(yieldTentative * 10000)/100f + "%");
                             System.out.println("Final Net Worth: " + NumberFormat.getCurrencyInstance().format(tentative_result));
@@ -66,7 +66,7 @@ public class Main {
 
                     // Didn't find a satisfactory answer but we still need to 
                     System.out.println("This calculation would take more than " + ITER_MAX + " iterations.");
-                    System.out.println("Estimated Yield: " + Math.round(yieldTentative * 100f) + "%");
+                    System.out.println("Estimated Yield: " + Math.round(yieldTentative * 10000)/100f + "%");
                     System.out.println("Final Net Worth: " + NumberFormat.getCurrencyInstance().format(tentative_result));
                     System.out.println("Monthly Dividends: " + NumberFormat.getCurrencyInstance().format(tentative_dividends));
                 }
@@ -81,10 +81,10 @@ public class Main {
                     for(int i=0; i<=ITER_MAX; i++){
                         CompoundProjector timeOBJ = new CompoundProjector(initial_Investment, salary, monthlySalaryShare, yield_investment, yield_dividend, timeTentative);
                         tentative_result = timeOBJ.projectNetWorth();
+                        tentative_dividends = timeOBJ.projectDividends();
                         
 
                         if (NEEDED_NETWORTH * 0.99999 < tentative_result && tentative_result < NEEDED_NETWORTH * 1.00001){ // Almost Equals, with 0.002% Threshold
-                            tentative_dividends = timeOBJ.projectDividends();
                             System.out.println("This calculation took " + i + " iterations.");
                             System.out.println("Estimated Years: " + Math.round(timeTentative * 100)/100f);
                             System.out.println("Final Net Worth: " + NumberFormat.getCurrencyInstance().format(tentative_result));
@@ -103,12 +103,44 @@ public class Main {
 
                     // Didn't find a satisfactory answer but we still need to 
                     System.out.println("This calculation would take more than " + ITER_MAX + " iterations.");
-                    System.out.println("Estimated Yield: " + Math.round(timeTentative * 100f) + "%");
+                    System.out.println("Estimated Years: " + Math.round(timeTentative * 100)/100f);
                     System.out.println("Final Net Worth: " + NumberFormat.getCurrencyInstance().format(tentative_result));
                     System.out.println("Monthly Dividends: " + NumberFormat.getCurrencyInstance().format(tentative_dividends));
                 }
                 break;
             case "Salary Share":
+                float shareFloor = 0.0f;
+                float shareCeil = 15f;
+                float shareTentative = 0.5f;
+
+                shareCase: {
+                    for(int i=0; i<=ITER_MAX; i++){
+                        CompoundProjector shareOBJ = new CompoundProjector(initial_Investment, salary, shareTentative, yield_investment, yield_dividend, investment_years);
+                        tentative_result = shareOBJ.projectNetWorth();
+                        tentative_dividends = shareOBJ.projectDividends();                        
+
+                        if (NEEDED_NETWORTH * 0.99999 < tentative_result && tentative_result < NEEDED_NETWORTH * 1.00001){ // Almost Equals, with 0.002% Threshold
+                            System.out.println("This calculation took " + i + " iterations.");
+                            System.out.println("Estimated Salary Share: " + Math.round(shareTentative * 10000)/100f + "%");
+                            System.out.println("Final Net Worth: " + NumberFormat.getCurrencyInstance().format(tentative_result));
+                            System.out.println("Monthly Dividends: " + NumberFormat.getCurrencyInstance().format(tentative_dividends));
+                            break shareCase;
+                        }
+                        // Didn't find the correct value, 'binary search' the new shareTentative
+                        if (tentative_result < NEEDED_NETWORTH){
+                            shareFloor = shareTentative;
+                            shareTentative = (shareFloor + shareCeil) / 2;
+                        }else{
+                            shareCeil = shareTentative;
+                            shareTentative = (shareFloor + shareCeil) / 2;
+                        }
+                    }                    
+                    // Didn't find a satisfactory answer but we still need to 
+                    System.out.println("This calculation would take more than " + ITER_MAX + " iterations.");
+                    System.out.println("Estimated Salary Share: " + Math.round(shareTentative * 10000)/100f + "%");
+                    System.out.println("Final Net Worth: " + NumberFormat.getCurrencyInstance().format(tentative_result));
+                    System.out.println("Monthly Dividends: " + NumberFormat.getCurrencyInstance().format(tentative_dividends));
+                }
                 break;
         }
     }
