d86f7cb6409d76a53cbf644bf434e08182420e06
GustavoSept <GustavoSept.Cron@gmail.com>
2023-10-15 18:51:07 -0300

    Fixing yield issue

--- calculator_FinancialFreedom/CompoundProjector.java
+++ calculator_FinancialFreedom/CompoundProjector.java
@@ -2,23 +2,25 @@ public class CompoundProjector{
     private long initial_Investment = 0;
     private float salary = 0f;
     private float monthlySalaryShare = 0f; // How much the user invests, as a % of the salary
-    private float yield = 0f;
+    private float yield_investment = 0f;
+    private float yield_dividend = 0f;
     private short investment_years = 0;
     
     private final byte MONTHS_IN_YEARS = 12;
 
 
-    CompoundProjector(long initial_Investment, float salary, float monthlySalaryShare, float yield, short investment_years){
+    CompoundProjector(long initial_Investment, float salary, float monthlySalaryShare, float yield_investment, float yield_dividend, short investment_years){
         this.initial_Investment = initial_Investment;
         this.salary = salary;
         this.monthlySalaryShare = monthlySalaryShare;
-        this.yield = yield;
+        this.yield_investment = yield_investment;
+        this.yield_dividend = yield_dividend;
         this.investment_years = investment_years;
     }
     
     public double projectNetWorth(){
         // FORMULA SHORTCUTS
-        final double RATE_OVER_MONTHS = this.yield / this.MONTHS_IN_YEARS;
+        final double RATE_OVER_MONTHS = this.yield_investment / this.MONTHS_IN_YEARS;
         final double ROM_POW = Math.pow(1+ RATE_OVER_MONTHS, this.MONTHS_IN_YEARS * this.investment_years);
         final float MONTHLY_INVESTMENT = this.salary * this.monthlySalaryShare;
 
@@ -28,7 +30,7 @@ public class CompoundProjector{
     }
 
     public double projectDividends(){
-        return projectNetWorth() * this.yield / this.MONTHS_IN_YEARS;
+        return projectNetWorth() * this.yield_dividend / this.MONTHS_IN_YEARS;
     }
 
     // Setters
@@ -47,8 +49,8 @@ public class CompoundProjector{
     }
 
 
-    public void setYield(float yield) {
-        this.yield = yield;
+    public void setYield(float yield_investment) {
+        this.yield_investment = yield_investment;
     }
 
 
--- calculator_FinancialFreedom/Main.java
+++ calculator_FinancialFreedom/Main.java
@@ -6,13 +6,14 @@ public class Main {
         long initial_Investment = 40000;
         float salary = 5000f;
         float monthlySalaryShare = 0.15f; // How much the user invests, as a % of the salary
-        float yield = 0.03f;
+        float yield_investment = 0.08f; // Yield Rate during investment period
+        float yield_dividend = 0.03f; // Yield Rate after conquering Financial Freedom (recommend between 0.02 ~0.05)
         short investment_years = 20;
         String operation = "Net Worth"; // possible values: "Salary Share", "Yield", "Time", "Net Worth"
         
 
         final byte MONTHS_IN_YEARS = 12;
-        double needed_netWorth = salary*MONTHS_IN_YEARS/yield; // The amount of money the user wishes to have at the end
+        double needed_netWorth = salary*MONTHS_IN_YEARS/yield_investment; // The amount of money the user wishes to have at the end
 
 
 
@@ -21,7 +22,7 @@ public class Main {
 
         switch (operation){
             case "Net Worth": // Calculating final Net Worth based on User Input
-                CompoundProjector netWorthObj = new CompoundProjector(initial_Investment, salary, monthlySalaryShare, yield, investment_years);
+                CompoundProjector netWorthObj = new CompoundProjector(initial_Investment, salary, monthlySalaryShare, yield_investment, yield_dividend, investment_years);
 
                 double result_end_Investment = netWorthObj.projectNetWorth();
 
@@ -41,7 +42,7 @@ public class Main {
 
                 yieldCase: {
                     for(int i=0; i<=ITER_MAX; i++){
-                        CompoundProjector yieldOBJ = new CompoundProjector(initial_Investment, salary, monthlySalaryShare, yieldTentative, investment_years);
+                        CompoundProjector yieldOBJ = new CompoundProjector(initial_Investment, salary, monthlySalaryShare, yieldTentative, yield_dividend, investment_years);
                         tentative_result = yieldOBJ.projectNetWorth();
                         tentative_dividends = yieldOBJ.projectDividends();
 
@@ -52,7 +53,7 @@ public class Main {
                             System.out.println("Monthly Dividends: " + NumberFormat.getCurrencyInstance().format(tentative_dividends));
                             break yieldCase;
                         }
-                        // Didn't find the correct value, 'binary search' the new yield
+                        // Didn't find the correct value, 'binary search' the new yield_investment
                         if (tentative_result < needed_netWorth){
                             yieldFloor = yieldTentative;
                             yieldTentative = (yieldFloor + yieldCeil) / 2;
